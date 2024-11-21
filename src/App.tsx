3d29709251eeb3c8cb758a596921fc33b7a21be8
il9strong <shitikow2016@yandex.ru>
2024-11-21 22:40:30 +0300

    fix: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from '@/components/HomePage/HomePage';
 import Favorites from '@/components/Favorites/Favorites';
 import Art from '@/components/Art/Art';
@@ -7,34 +7,32 @@ import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<ErrorBoundary>
-							<HomePage />
-						</ErrorBoundary>
-					}
-				/>
-				<Route
-					path="/favorites"
-					element={
-						<ErrorBoundary>
-							<Favorites />
-						</ErrorBoundary>
-					}
-				/>
-				<Route
-					path="art/:id"
-					element={
-						<ErrorBoundary>
-							<Art />
-						</ErrorBoundary>
-					}
-				/>
-			</Routes>
-		</Router>
+		<Routes>
+			<Route
+				path="/"
+				element={
+					<ErrorBoundary>
+						<HomePage />
+					</ErrorBoundary>
+				}
+			/>
+			<Route
+				path="/favorites"
+				element={
+					<ErrorBoundary>
+						<Favorites />
+					</ErrorBoundary>
+				}
+			/>
+			<Route
+				path="art/:id"
+				element={
+					<ErrorBoundary>
+						<Art />
+					</ErrorBoundary>
+				}
+			/>
+		</Routes>
 	);
 }
 
