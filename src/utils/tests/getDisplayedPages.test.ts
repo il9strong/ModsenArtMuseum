820357f9030103a8fe1c6859dfc8de19ea258c7b
il9strong <shitikow2016@yandex.ru>
2024-11-22 14:43:53 +0300

    fix: test

--- src/utils/tests/getDisplayedPages.test.ts
+++ src/utils/tests/getDisplayedPages.test.ts
@@ -1,15 +1,15 @@
 import { getDisplayedPages } from '@/utils/getDisplayedPages';
 
 describe('getDisplayedPages', () => {
-	test('должен возвращать правильные страницы, когда текущая страница посередине', () => {
+	test('should return correct pages when current page is in the middle', () => {
 		expect(getDisplayedPages(3, 10)).toEqual([1, 2, 3, 4]);
 	});
 
-	test('должен корректно обрабатывать случай, когда текущая страница ближе к началу', () => {
+	test('should correctly handle the case where the current page is closer to the beginning', () => {
 		expect(getDisplayedPages(1, 10)).toEqual([1, 2, 3, 4]);
 	});
 
-	test('должен корректно обрабатывать случай, когда текущая страница ближе к концу', () => {
+	test('should correctly handle the case when the current page is near the end', () => {
 		expect(getDisplayedPages(9, 10)).toEqual([7, 8, 9, 10]);
 	});
 });
